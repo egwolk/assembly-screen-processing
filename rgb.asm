6992fd9c533969adaff036f706fccc38bb9701b3
egwolk <erincovacha@gmail.com>
2026-02-26 10:07:35 +0800

    refactor assembly code to properly display message

--- rgb.asm
+++ rgb.asm
@@ -1,9 +1,14 @@
 .model small
 .stack 100h
 .data
+    msg db "Hello World Test $"
 .code
     mov ax, @data
     mov ds, ax
+
+    mov ah, 9
+    lea dx, msg
+    int 21h
 
     mov ah, 4ch
     int 21h
