abbda02a17f5699fae26a1978933dc6d8b73f623
egwolk <erincovacha@gmail.com>
2026-02-26 10:58:24 +0800

    add RGB color display functionality to assembly code

--- rgb.asm
+++ rgb.asm
@@ -1,14 +1,37 @@
 .model small
 .stack 100h
 .data
-    msg db "Hello World Test $"
 .code
     mov ax, @data
     mov ds, ax
 
-    mov ah, 9
-    lea dx, msg
-    int 21h
+    mov ah, 00h
+    mov al, 03
+    int 10h
+
+    ;red
+    mov ah, 6 ;scroll up
+    mov al, 00h
+    mov bh, 40h ;4 = red bg
+    mov cx, 0000h ;row 00, col 00
+    mov dx, 074fh ;last row 18 = 24, lasst col 4f = 79
+    int 10h
+
+    ;green
+    mov ah, 6 ;scroll up
+    mov al, 00h
+    mov bh, 20h ;2 = green bg
+    mov cx, 0800h 
+    mov dx, 0f4fh
+    int 10h
+
+    ;blue
+    mov ah, 6 ;scroll up
+    mov al, 00h
+    mov bh, 90h ;9 = blue bg
+    mov cx, 1000h 
+    mov dx, 184fh
+    int 10h
 
     mov ah, 4ch
     int 21h
